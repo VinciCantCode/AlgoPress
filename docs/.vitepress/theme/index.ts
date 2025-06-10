05acbfbaf01869be6d0085e41fe45a400d59ba28
Vinci <Wenxiliu77@gmail.com>
2025-06-10 14:12:25 -0700

    Update + 1

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 
 if (typeof window !== 'undefined') {
   inject()
+  injectSpeedInsights()
 }
 
 export default {
