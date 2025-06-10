e9726abb6590f61dc789c49dbc21f144f7b5a44d
Vinci <Wenxiliu77@gmail.com>
2025-06-10 14:11:27 -0700

    Update Vercel Analytics and Speed Insights

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,10 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
+import { inject } from '@vercel/analytics'
+
+if (typeof window !== 'undefined') {
+  inject()
+}
 
 export default {
   ...DefaultTheme
