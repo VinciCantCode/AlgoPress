ecec005e2d89dae108d2fcd2a235a193cfd292c9
Vinci <Wenxiliu77@gmail.com>
2025-06-23 20:08:09 -0700

    Add new comment feature;  Integrate postgresql

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,12 +2,20 @@ import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 import { inject } from '@vercel/analytics'
 import { injectSpeedInsights } from '@vercel/speed-insights'
+import CommentSection from '../../components/CommentSection.vue'
+import UserList from '../../components/UserList.vue'
 
 if (typeof window !== 'undefined') {
   inject()
   injectSpeedInsights()
 }
 
+/** @type {import('vitepress').Theme} */
 export default {
-  ...DefaultTheme
+  extends: DefaultTheme,
+  enhanceApp({ app }) {
+    // Global Component Registration
+    app.component('CommentSection', CommentSection)
+    app.component('UserList', UserList)
+  }
 } 
--- docs/test-users.md
+++ docs/test-users.md
@@ -1,17 +1,14 @@
 ---
 title: User List Test
 ---
-<script setup>
-import UserList from './components/UserList.vue'
-import CommentSection from './components/CommentSection.vue'
-</script>
+<!-- <script setup>
+import UserList from '@components/UserList.vue'
+import CommentSection from '@components/CommentSection.vue'
+</script> -->
 
 # Supabase Connection Test
 
 If the component below successfully displays a list of users, it means your Vue component is fetching data from Supabase correctly.
 
 <UserList /> 
-
----
-
 <CommentSection /> 
